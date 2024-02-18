c73f246517884046caa35e67d28e530c5c51f553
Ravneet Anand <anand.ravneet099@gmail.com>
2024-02-18 22:43:36 +0000

    FIlter out the time data-type column from arithmetic operations.

--- src/components/FormulaBuilder/TableColumns.tsx
+++ src/components/FormulaBuilder/TableColumns.tsx
@@ -9,10 +9,15 @@ type TableColumnsProps = {
 export const TableColumns: React.FC<TableColumnsProps> = ({ addToFormula }) => {
   const { columns } = useContext(ColumnsContext);
 
+  // Filter out the time data type columns
+  const filteredColumns = columns.filter(
+    ({ columnType }) => columnType !== 'time',
+  );
+
   return (
     <SubContainer>
       <h3>Table Columns</h3>
-      {columns.map(({ columnName }, index) => (
+      {filteredColumns.map(({ columnName }, index) => (
         <ColumnNameButton key={index} onClick={addToFormula} outlined={true}>
           {columnName}
         </ColumnNameButton>
